bf7d71abd4d92c391dbf7ddd414367a79ad53331
BSDinis <baltasardinis@hotmail.com>
2019-05-29 13:04:00 +0100

    updated macros

--- common/macros.c
+++ common/macros.c
@@ -7,9 +7,8 @@
 
 void error_kill(const char *file, int lineno, const char *msg)
 {
-  fprintf(stderr, "%s:%d %s\n", file, lineno, msg);
-  ERR_print_errors_fp(stderr);
-  exit(-1);
+  error_log(file, lineno, msg);
+  exit(EXIT_FAILURE);
 }
 
 void error_log(const char *file, int lineno, const char *msg)
