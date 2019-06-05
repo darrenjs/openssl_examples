88edc798414e386cf3be01a62a020b2bb476bce5
BSDinis <baltasardinis@hotmail.com>
2019-06-05 21:41:22 +0100

    updated config

--- common/config.h
+++ common/config.h
@@ -9,3 +9,5 @@ const char server_cert_path[] = "server.crt";
 const char server_key_path[]  = "server.key";
 const char client_cert_path[] = "client.crt";
 const char client_key_path[]  = "client.key";
+
+const char root_ca[] = "rootCA.pem";
